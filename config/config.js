729f4f74341838c8d938002ec07d342ce1ad9edc
fotis_pozoglou <fotispozoglou@protonmail.com>
2022-07-30 00:57:53 +0300

    Configuring For Heroku Deployment

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
-module.exports.CLIENT_DOMAIN = 'https://efood-client.herokuapp.com';
-module.exports.SERVER_DOMAIN = 'https://efoodapi.herokuapp.com';
-module.exports.ADMIN_DOMAIN = 'https://efood-admin.herokuapp.com';
+module.exports.IS_PRODUCTION = process.env.NODE_ENV === "production";
+const LOCAL_IP = '192.168.1.8';
+module.exports.CLIENT_DOMAIN = this.IS_PRODUCTION ? 'https://efood-client.herokuapp.com' : `http://${ LOCAL_IP }:8000`;
+module.exports.SERVER_DOMAIN = this.IS_PRODUCTION ? 'https://efoodapi.herokuapp.com' : `http://${ LOCAL_IP }:3000`;
+module.exports.ADMIN_DOMAIN = this.IS_PRODUCTION ? 'https://efood-admin.herokuapp.com' : `http://${ LOCAL_IP }:8080`;
 module.exports.SERVER_URL = `${ this.SERVER_DOMAIN }`;
